b7335561311304ce25e7850a77bbf40182068fc3
free jacow gwn <jakovmarkovanovic@gmail.com>
2020-03-26 20:58:12 +0100

    Add ClaamAchimentsCommand

---
+++ src/ClashRoyale/Protocol/Commands/Client/ClaamAchimentsCommand.cs
@@ -0,0 +1,33 @@
+﻿using ClashRoyale.Logic;
+using ClashRoyale.Protocol.Messages.Server;
+using ClashRoyale.Utilities.Netty;
+using DotNetty.Buffers;
+
+namespace ClashRoyale.Protocol
+{
+    internal class ClaamAchimentsCommand : LogicCommand
+    {
+        public ClaamAchimentsCommand(Device device, IByteBuffer buffer) : base(device, buffer)
+        {
+        }
+
+        public override void Decode()
+        {
+            base.Decode();
+
+            Reader.ReadVInt();//67
+            Reader.ReadVInt();//67
+            Reader.ReadVInt();//0
+            Reader.ReadVInt();//7
+        }
+
+        public override async void Process()
+        {
+            await new ServerErrorMessage(Device)
+            {
+                Message = "Not implemented yet."
+            }.SendAsync();
+        }
+    }
+
+}
